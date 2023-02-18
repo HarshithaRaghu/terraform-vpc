797d404d172a78feb4da81650b22b87522b354ba
Harshitha <harshithadarshan95@gmail.com>
2023-02-18 13:27:51 +0530

    Autopush

--- main.tf
+++ main.tf
@@ -17,6 +17,9 @@ module "vpc" {
   AZ                   = var.AZ   
   PUBLIC_SUBNET_CIDR   = var.PUBLIC_SUBNET_CIDR
   PRIVATE_SUBNET_CIDR  = var.PRIVATE_SUBNET_CIDR
+  DEFAULT_VPC_CIDR     = var.DEFAULT_VPC_CIDR
+  DEFAULT_VPC_ID       = var.DEFAULT_VPC_ID
+  DEFAULT_VPC_RT       = var.DEFAULT_VPC_RT
 
 }
 
